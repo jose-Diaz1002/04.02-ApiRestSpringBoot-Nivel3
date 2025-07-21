2bf4c92866bc433fad0a94a66a08639dcd7e96dd
jose-Diaz1002 <128887448+jose-Diaz1002@users.noreply.github.com>
2025-07-22 00:23:03 +0200

    Initial commit

---
+++ src/main/java/cat/itacademy/s04/t02/n03/S04T02N03/model/Fruit.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s04.t02.n03.S04T02N03.model;
+
+public class Fruit {
+}
---
+++ src/main/java/cat/itacademy/s04/t02/n03/S04T02N03/services/impl/FruitServiceImpl.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s04.t02.n03.S04T02N03.services.impl;
+
+public class FruitServiceImpl {
+}
